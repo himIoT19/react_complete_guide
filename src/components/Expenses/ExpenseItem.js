ed7a5b871730b5417a4f80e8a454a7a0530d4584
himIoT19 <thehimu1990@gmail.com>
2021-07-18 07:41:49 +0530

    Working with 'State', and closer look at the 'useState' Hook.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // array destructuring
+  const [title, setTitle] = useState(props.title); // 'useState' REACT Hook(can't be called at other places), return exactly 2 variables
+  /*
+  1. title(first returned value): Is the current state value
+  2. setTitle(): function for updating the above current state value(title)
+  */
+
   // Attached to event-listener => "onClick"
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    setTitle("Updated!");
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickHandler}>Changed Title</button>
     </Card>
   );
 };
