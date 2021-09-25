fa8e0d971cec9c5e91ab787ca7a4838d92fa7434
himIoT19 <thehimu1990@gmail.com>
2021-09-25 17:59:58 +0530

    Using Stateful Lists

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // array destructuring
-  const [title, setTitle] = useState(props.title); // 'useState' REACT Hook(can't be called at other places), return exactly 2 variables
-  /*
-  1. title(first returned value): Is the current state value
-  2. setTitle(): function for updating the above current state value(title)
-  */
-
-  // Attached to event-listener => "onClick"
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">₹{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Changed Title</button>
     </Card>
   );
 };
